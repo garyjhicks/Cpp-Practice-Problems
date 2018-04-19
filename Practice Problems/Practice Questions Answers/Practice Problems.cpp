39c47a2e79f8d6fa794eb557202f1f046e48e679
Gary Jinghua Hicks <h_gary@outlook.com>
2018-04-19 14:05:56 -0400

    Delete Practice Problems.cpp

--- Practice Problems/Practice Questions Answers/Practice Problems.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Practice Problems.cpp
-//  
-//
-//  Created by Ellen Choi on 2017-12-09.
-//
-
-#include "Practice Problems.hpp"
-/Users/eunseochoi/Library/Mobile Documents/com~apple~CloudDocs/1A/GENE 121/Practice Problems/Practice Questions Answers/Question2.cpp
